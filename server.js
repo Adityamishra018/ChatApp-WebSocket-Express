426d84d538579f1267a5711e9d3301bda0ecba08
Aditya Mishra <adi.adityamishra007@gmail.com>
2024-04-28 23:27:38 +0530

    chat feature added

--- server.js
+++ server.js
@@ -13,13 +13,15 @@ app.use(cors())
 app.set('view engine','ejs')
 app.set('views',path.resolve('./views'))
 
-app.use(Express.static("."))
+app.use(Express.static("public"))
 
 app.get('/',(req,res)=>{
     res.render('index',{
         address : process.env.WEBURL
     });
 })
+
+const usernames = ["Muggle Mischief", "Lumos Luna", "Wizarding Whiz", "Snape Sorcerer", "Patronus Pixie", "Quidditch Quest", "Diagon Daring", "Gryffin Giggles", "Hagrids Helper", "Azkaban Escapee"]
 
 const server = app.listen(process.env.PORT,(err)=>{
     if(!err)
@@ -34,8 +36,28 @@ const io = new Server(server,{
             })
 
 io.on('connection',(socket)=>{
-    socket.on('newMsgFromClient',msg=>{
-        console.log('received msg', msg)
-        io.emit('newMsgFromServer',msg)
-    })
+    let user = usernames.pop()
+    if (user){
+        socket.emit('username',user)
+
+        console.log('connected',user)
+        console.log('Users on chat : ',10 - usernames.length)
+
+        io.emit('newUser',user)
+        
+        socket.on('newMsgFromClient',({msg,sender})=>{
+            io.emit('newMsgFromServer',{msg,sender})
+        })
+
+        socket.on('disconnect',()=>{
+            console.log('Connection closed with : ',user)
+            usernames.push(user)
+            io.emit('lostUser',user)
+            console.log('Users on chat : ',10 - usernames.length)
+
+        })
+    }
+    else{
+        socket.disconnect()
+    }
 })
